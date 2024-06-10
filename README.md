4c65dbea5d6abc8e5445fa934c653af5b8ece67d
J4nn2s <jannesglaubitz@gmail.com>
2024-06-10 17:17:26 +0200

    data loading

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BI-Project
+
+Die Excel Datei für das Projekt im übergeordneten Verzeichnis platzieren, um die Daten korrekt zu laden.
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import os
+import pandas as pd
 if __name__ == "__main__":
-    print("        ____")
-    print("       /    \\")
-    print("      |      |")
-    print("      |      |")
-    print("      |      |")
-    print("      |      |")
-    print("     /        \\")
-    print(" ___|          |___")
-    print("|                  |")
-    print(" \\________________/")
+    parent_directory = os.path.dirname(os.getcwd())
+
+    file_path = os.path.join(parent_directory, "Crimes_2012-2016.csv")
+    data = pd.read_csv(file_path, sep=",")
+    print(data.info())
